86556e0500afe5357b5f5621e0e36ff83d9e3882
SunJar99 <beshkempirov.1966@gmail.com>
2025-04-08 23:56:19 +0600

    everything

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def text_response_view(request):
+    return HttpResponse("Привет! Это текстовый ответ.")
+
+def html_template_view(request):
+    return render(request, 'myapp/sample.html')
